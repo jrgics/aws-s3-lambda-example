0276fab4260ed920c606ea5521d3128f869065dc
John Ghidiu <john@rmdashrf.org>
2019-06-02 12:02:27 -0400

    Runtime enhancements

--- test/server-util/run-server.js
+++ test/server-util/run-server.js
@@ -6,27 +6,43 @@
 // [X] 5.) Get postman to run against multiple environments
 // [X] 6.) Get cypress to run cli against local serve envrionment w/ startup & shutdown
 // [X] 7.) Get postman to run cli against local serve environment w/ startup & shutdown
-// [ ] 8.) Integrate tests into package scripts
+// [X] 8.) Integrate tests into package scripts
 // [ ] 9.) Incorporate into build/ci/cd
-
-// TODO: npm target to run test server (and reload on change)
 
 let chalk = require('chalk');
 let express = require('express');
 let app = express();
 
-exports.runServer = function runServer(onServerStarted) {
+// Default port
+const DEFAULT_PORT = 3000;
 
+/**
+ * Runs the server.
+ *
+ * @param {Function} the callback once the server has started (receives a config object)
+ * @param {Number} [listenPort] the port to listen on
+ */
+exports.runServer = function runServer(onServerStarted, listenPort) {
+
+    let port = listenPort || DEFAULT_PORT;
+
+    /**
+     * Middleware to shutdown the server (perhaps used by manual scripts).
+     */
     app.get('/shutdown', (request, response) => {
-        response.json({message: 'ok'});
+        response.json({ message: 'ok' });
         stopServer();
     });
 
+    /**
+     * Middleware to execute an API function (lambda).
+     */
     app.get('/api/:name', (request, response, next) => {
 
         // Delegate to the lambda with the matching "name"
         //
-        // Create an 'event' for the lambda to consume
+        // Create an 'event' for the lambda to consume (this might have to be
+        // enhanced if the lambdas require more information in the event)
         let event = {
             queryStringParameters: request.query
         };
@@ -36,47 +52,80 @@ exports.runServer = function runServer(onServerStarted) {
 
         // Invoke the lambda
         lambda(event)
-        .then(lambdaResponse => {
-            /// Process the results
-            Object.keys(lambdaResponse.headers).forEach(key => {
-                response.setHeader(key, lambdaResponse.headers[key]);
-            });
-            response.status(lambdaResponse.statusCode);
-            response.json(JSON.parse(lambdaResponse.body));
+            .then(lambdaResponse => {
+                // Process the results
+                //
+                // This might have to be revisited if the lambdas modify the response more
+                Object.keys(lambdaResponse.headers).forEach(key => {
+                    response.setHeader(key, lambdaResponse.headers[key]);
+                });
+                // Set the status code
+                response.status(lambdaResponse.statusCode);
+                // Set the body
+                response.json(JSON.parse(lambdaResponse.body));
 
-            return next();
-        });
+                // Invoke the next middleware
+                return next();
+            });
 
     });
 
     // Serve static content
     app.use(express.static('app/html'));
 
+    /**
+     * Stops the server.
+     *
+     * @returns {Promise} resolves when the server stops, rejects on stop error
+     */
     function stopServer() {
-        console.info(chalk.blue('shutting down'));
-        server.close(error => {
-            if (!!error) {
-                console.error(chalk.red('server close error'));
-                console.error(chalk.red(error));
-            } else {
-                console.info(chalk.blue('server terminated'));
-            }
+        return new Promise((resolve, reject) => {
+            console.info(chalk.blue('shutting down'));
+            server.close(error => {
+                if (!!error) {
+                    console.error(chalk.red('server close error'));
+                    console.error(chalk.red(error));
+                    reject(error);
+                } else {
+                    console.info(chalk.blue('server terminated'));
+                    resolve();
+                }
+            });
         });
     }
 
-    let server = app.listen(3000, null, () => {
+    // Create the server instance
+    let server = app.listen(port, null, () => {
         console.info(chalk.blue('starting server'));
 
-        onServerStarted()
-        .then(() => {
-            console.log(chalk.green('server tests finished'));
-        })
-        .catch(error => {
-            console.error(chalk.red('error during server test:', error));
-        })
-        .then(stopServer);
+        // The exit code
+        let exitCode = 0;
 
-        console.info(chalk.blue('running test server on port 3000'));
+        // The server config
+        let serverConfig = {
+            port: port,
+            protocol: 'http',
+            hostname: 'localhost'
+        }
+
+        // Start the promise chain
+        onServerStarted(serverConfig)
+            .then(() => {
+                // Handle normal case
+                console.log(chalk.green('server finished'));
+            })
+            .catch(error => {
+                // Handle exceptional case
+                console.error(chalk.red('error during server run:', error));
+                // Mark the exit as a failure (in case this process is used with testing)
+                exitCode = 1;
+            })
+            .then(stopServer)
+            .catch((/*error*/) => /*Swallow the error*/ null)
+            .then(() => process.exit(exitCode));
+
+        // Notify the consumer that the server has finished running
+        console.info(chalk.blue(`running server on port ${port}`));
     });
 
 }
